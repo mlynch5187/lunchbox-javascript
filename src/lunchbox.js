88ab5d7e0031df76494345463fa2e3a0f7dd48b8
Michael Alex Lynch <mlynch5187@gmail.com>
2021-03-08 12:31:54 -0700

    Lunchoxes have materials

--- src/lunchbox.js
+++ src/lunchbox.js
@@ -2,6 +2,7 @@ class Lunchbox  {
   constructor(obj) {
     obj = obj || {};
     this.owner = obj.owner
+    this.material = obj.madeOf;
   }
 }
 
--- test/lunchbox-test.js
+++ test/lunchbox-test.js
@@ -12,7 +12,7 @@ describe('Lunchbox', function() {
     assert.equal(khalidsBox.owner, "Khalid");
   });
 
-  it.skip('should have a material', function() {
+  it('should have a material', function() {
     var willsBox = new LunchBox({ owner: "Will", madeOf: "metal", shape: "rectangular prism", color: "multi" });
     var khalidsBox = new LunchBox({ owner: "Khalid", madeOf: "plastic", shape: "rectangular prism", color: "clear" });
 
